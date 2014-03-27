ae16a26700179fa76b78f3167eab3dfc3df5454c
Vasili <cetanu@gmail.com>
2014-03-27 23:06:46 +1100

    fixed some output and split the DNSBL variable.

--- blistd.ps1
+++ blistd.ps1
@@ -1,20 +1,19 @@
-# IP ADDRESS TO CHECK
+# IP Address Settings
+$ipaddress = "127.0.0.1"  # We are checking this address
+if ($ipaddress -eq $null -or $ipaddress -match "^(192|172|10)")
+{
+	Write-Error "A valid, public IP address is required for this script to work. Please check your configuration."
+	exit
+}
+$ipaddress -match "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" | Out-Null
+$reverse_ip = "$($Matches[4]).$($Matches[3]).$($Matches[2]).$($Matches[1])"  # Reverse each capture group
 
-$ip = "127.0.0.1"  # This should be a public address
-
-
-# EMAIL SETTINGS
-
-# Server details
-$smtpserver = "smtp.email.com"
-$Username = "username"
-$Password = "password"
-
-# Address details
+# Email Settings
+$email_server = "smtp.email.com"
+$email_username = "username"
+$email_password = "password"
 $email_from = "from@email.com"
 $email_to = "to@email.com"
-
-# Message details
 $email_subject = "$($env:computername) detected on $($BL) DNS Blacklist"
 $email_body = "
 Please be advised that $($env:computername)
@@ -23,25 +22,8 @@ Sincerely,
 Your mate
 "
 
-
-# OBTAINING REVERSE IP
-
-# Check IP address to ensure it is public
-if ($ip -eq $null -or $ip -match "^(192|172|10)")
-{
-	Write-Error "A valid, public IP address is required for this script to work. Please check your configuration."
-	exit
-}
-
-$ip -match "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" | Out-Null
-
-$reverse_ip = "$($Matches[4]).$($Matches[3]).$($Matches[2]).$($Matches[1])"  # Reverse each capture group
-
-
-# FUNCTIONS
-
-# Emailing
-function alert($BL)
+# Functions
+function email_alert($BL)
 {	
 	# Message Object Creation
 	$message = new-object System.Net.Mail.MailMessage
@@ -51,30 +33,25 @@ function alert($BL)
 	$message.body = $email_body
 	
 	# Server Connection Object Creation
-	$smtp = new-object Net.Mail.SmtpClient($smtpserver)
-	$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password)
+	$smtp = new-object Net.Mail.SmtpClient($email_server)
+	$smtp.Credentials = New-Object System.Net.NetworkCredential($email_username, $email_password)
 	$smtp.Send($message)
 }
-
-# Logging
-function log ($string)
+function log($string)
 {
 	(date -format "HH:mm:sstt, dd MMM yyyy | ") + $string | Out-file ".\blistd.log" -a -en ASCII
 	Write-Output $string
 }
 
+# Blacklists
+Write-Output "Updating DNSBLs..."
+(Invoke-WebRequest 'https://gist.github.com/cetanu/9697771').content | ? {$_ -match '(?<=View Raw" href=")[^"]*'} | Out-Null
+$URL = $Matches[0]
+$DNSBL = ((Invoke-WebRequest $URL).content -split "\n")  # Automatically download a list of DNSBLs from Gist
+Write-Output "Done.`n"
 
-# BLACKLISTS
-
-(Invoke-WebRequest 'https://gist.github.com/cetanu/9697771').content | ? {$_ -match '(?<=View Raw" href=")[^"]*'}
-$url = $Matches[0]
-$DNSBL = (Invoke-WebRequest $url).content  # Automatically download a list of DNSBLs from Gist
-
-
-# MAIN
-
+# Begin checking
 $ErrorActionPreference = "SilentlyContinue"  # Ignore errors, we are supposed to get nslookup failures
-
 While (1)
 {
 	ForEach ($BL in $DNSBL)
@@ -89,7 +66,7 @@ While (1)
 		Else
 		{
 			log "LISTED-`t$($reverse_ip).$($BL)"
-			alert($BL)
+			email_alert($BL)
 		}
 	}
 	Sleep (Random -min 60 -max 14000)  # delay to avoid looking like a bot
